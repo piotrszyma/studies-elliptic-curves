85d0f6d79b197cbc2af176d79138666bd7ff8f8c
Piotr Szyma <thompson2908@gmail.com>
2020-04-17 23:49:44 +0200

    AffinePoint impl added

--- list_2/py/structures/affine.py
+++ list_2/py/structures/affine.py
@@ -2,16 +2,51 @@ import collections
 import random
 import copy
 import math
+import dataclasses
 
 from typing import Optional
 
+CurveBasePoint = collections.namedtuple("CurveBasePoint", "x y")
 
-CURVE_ORDER = 807369655039
-CURVE = "y^2 = x^3 + 236367012452*x + 74315650609"
-base_point = collections.namedtuple("base_point", "x y")
-CURVE_PARAM_A = 236367012452
-CURVE_PARAM_B = 74315650609
-CURVE_BASE_POINT = base_point(172235452673, 488838007757)
+
+@dataclasses.dataclass
+class CurveParams:
+    base_point: CurveBasePoint
+    a: int
+    b: int
+    curve_order: int
+    field_order: int
+
+
+_CURVE_PARAM_ORDER = 807369655039
+_CURVE_PARAM_FIELD_ORDER = 807368793739
+_CURVE_PARAM_A = 236367012452
+_CURVE_PARAM_B = 74315650609
+_CURVE_PARAM_BASE_POINT = CurveBasePoint(172235452673, 488838007757)
+
+
+def set_curve_params(curve_params: CurveParams):
+    global _CURVE_PARAM_BASE_POINT
+    global _CURVE_PARAM_ORDER
+    global _CURVE_PARAM_FIELD_ORDER
+    global _CURVE_PARAM_A
+    global _CURVE_PARAM_B
+    _CURVE_PARAM_BASE_POINT = curve_params.base_point
+    _CURVE_PARAM_ORDER = curve_params.curve_order
+    _CURVE_PARAM_FIELD_ORDER = curve_params.field_order
+    _CURVE_PARAM_A = curve_params.a
+    _CURVE_PARAM_B = curve_params.b
+
+
+def modinv(a, n):
+    b, c = 1, 0
+    while n:
+        q, r = divmod(a, n)
+        a, b, c, n = n, c, b - q * c, r
+    # at this point a is the gcd of the original inputs
+    if a == 1:
+        return b
+    raise ValueError("Not invertible")
 
 
 class AffinePoint:
@@ -19,14 +54,19 @@ class AffinePoint:
     _base_point = None
     __slots__ = ("x", "y")
 
-    def __init__(self, x: Optional[int] = None, y: Optional[int] = None):
-        self.x = x
-        self.y = y
+    def __init__(
+        self,
+        x: Optional[int] = None,
+        y: Optional[int] = None,
+        inf: Optional[bool] = False,
+    ):
+        self.x = x % _CURVE_PARAM_FIELD_ORDER if x else x
+        self.y = y % _CURVE_PARAM_FIELD_ORDER if y else y
 
     def __repr__(self):
         if self.is_infinity():
-            return "AffinePoint<at infinity>"
-        return f"AffinePoint<{self.x} {self.y}>"
+            return "AffinePoint(at infinity)"
+        return f"AffinePoint({self.x}, {self.y})"
 
     def __eq__(self, other: "AffinePoint") -> bool:
         if not isinstance(other, AffinePoint):
@@ -41,6 +81,8 @@ class AffinePoint:
         if not isinstance(value, int):
             raise NotImplementedError(f"Cannot multiply {type(self)} and {type(value)}")
 
+        value = value % _CURVE_PARAM_ORDER
+
         if value == 2:
             if self.is_infinity():
                 return self.get_infinity()
@@ -48,23 +90,27 @@ class AffinePoint:
             if self.y == 0:
                 return self.get_infinity()
 
-            s = (3 * (self.x ** 2) + CURVE_PARAM_A) // (2 * self.y)
-            x_ = s ** 2 - 2 * self.x
-            y_ = s * (self.x - x_) - self.y
-            return AffinePoint(x=x_, y=y_)
+            s = (
+                pow(self.x, 2, _CURVE_PARAM_FIELD_ORDER) * 3 + _CURVE_PARAM_A
+            ) * modinv(2 * self.y, _CURVE_PARAM_FIELD_ORDER)
+            x2 = pow(s, 2, _CURVE_PARAM_FIELD_ORDER) - 2 * self.x
+            y2 = (s * (self.x - x2)) - self.y
 
-        result = copy.deepcopy(self)
+            return AffinePoint(x=x2, y=y2)
 
-        no_muls = math.ceil(math.sqrt(value))
-        no_adds = value - 2 ** no_muls
+        temp = copy.deepcopy(self)
+        result = AffinePoint.get_infinity()
 
-        for _ in range(no_muls):
-            result *= 2
-
-        for _ in range(no_adds):
-            result += self
+        while value != 0:
+            if value & 1 != 0:
+                result += temp
+            temp *= 2
+            value >>= 1
 
         return result
+
+    def __rmul__(self, other):
+        return self.__mul__(other)
 
     def __add__(self, other: "AffinePoint") -> "AffinePoint":
         if not isinstance(other, AffinePoint):
@@ -81,19 +127,22 @@ class AffinePoint:
                 return self * 2
             return AffinePoint.get_infinity()
 
-        s = (self.y - other.y) // (self.x - other.x)
+        s = (self.y - other.y) * modinv(self.x - other.x, _CURVE_PARAM_FIELD_ORDER)
 
         x_ = s ** 2 - self.x - other.x
         y_ = s * (self.x - x_) - self.y
 
         return AffinePoint(x=x_, y=y_)
 
+    def __radd__(self, other):
+        return self.__add__(other)
+
     def is_infinity(self):
         return self.x is None and self.y is None
 
     @classmethod
     def random(cls):
-        return cls.get_base_point() * random.randint(2, CURVE_ORDER)
+        return cls.get_base_point() * random.randint(2, _CURVE_PARAM_ORDER)
 
     @classmethod
     def get_infinity(cls):
@@ -106,5 +155,5 @@ class AffinePoint:
     def get_base_point(cls):
         if cls._base_point:
             return cls._base_point
-        cls._base_point = cls(*CURVE_BASE_POINT)
+        cls._base_point = cls(*_CURVE_PARAM_BASE_POINT)
         return cls._base_point
---
+++ list_2/py/structures/affine_test.py
@@ -0,0 +1,124 @@
+import copy
+import unittest
+
+from affine import AffinePoint, set_curve_params, CurveBasePoint, CurveParams
+
+
+class AffinePointTestCase(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        set_curve_params(
+            CurveParams(
+                base_point=CurveBasePoint(172235452673, 488838007757),
+                a=236367012452,
+                b=74315650609,
+                field_order=807368793739,
+                curve_order=807369655039,
+            )
+        )
+
+class TestAffinePointMultiplicationByScalar(AffinePointTestCase):
+    def test_point_at_infinitity_times_scalar_should_be_infinity(self):
+        # Arrange.
+        point_at_inf = AffinePoint.get_infinity()
+
+        # Act.
+        result = point_at_inf * 2
+
+        # Assert.
+        self.assertEqual(result, point_at_inf)
+
+    def test_point_with_y_equal_zero_multiplied_should_be_infinity(self):
+        # Arrange.
+        # Ugly sets y to 0 without respecting x.
+        point_with_y_zero = AffinePoint(0, 0)
+
+        # Act.
+        result = point_with_y_zero * 2
+
+        # Assert.
+        self.assertTrue(result.is_infinity())
+
+
+class TestAffinePointAddition(AffinePointTestCase):
+    def test_point_at_infinity_plus_other_should_be_that_other(self):
+        # Arrange.
+        point = AffinePoint(1234, 5678)
+        point_at_inf = AffinePoint.get_infinity()
+
+        # Act.
+        result_rhs = point + point_at_inf
+        result_lhs = point_at_inf + point
+
+        # Assert.
+        self.assertEqual(result_lhs, result_rhs)
+        self.assertEqual(result_rhs, point)
+
+    def test_two_points_with_same_x_should_sum_to_infinity(self):
+        # Arrange.
+        # Ugly sets xs to same value.
+        point_1 = AffinePoint(1234, 1234)
+        point_2 = AffinePoint(1234, 5678)
+
+        # Act.
+        result = point_1 + point_2
+
+        # Assert.
+        self.assertEqual(result, AffinePoint.get_infinity())
+
+    def test_two_points_with_same_x_and_same_y(self):
+        # Arrange.
+        point_1 = AffinePoint(172235452673, 488838007757)
+        point_2 = AffinePoint(172235452673, 488838007757)
+
+        # Act.
+        result = point_1 + point_2
+
+        # Assert.
+        self.assertEqual(result, AffinePoint(215387987039, 765000578277))
+
+    def test_mix_of_add_and_mul(self):
+        # Arrange.
+        point = AffinePoint(172235452673, 488838007757)
+
+        # Act.
+        two_mul_point = 2 * point
+        point_add_point = point + point
+
+        # Assert.
+        self.assertEqual(two_mul_point, AffinePoint(215387987039, 765000578277))
+        self.assertEqual(two_mul_point, point_add_point)
+
+    def test_mul(self):
+        # Arrange.
+        point = AffinePoint(172235452673, 488838007757)
+
+        # Act.
+        point_mul_0 = 2 * point
+        point_mul_1 = 2137 * point
+        point_mul_2 = 741274052018 * point
+        point_mul_3 = 649074375334 * point
+
+        # Assert.
+        self.assertEqual(AffinePoint(215387987039, 765000578277), point_mul_0)
+        self.assertEqual(AffinePoint(464122625441, 361301908555), point_mul_1)
+        self.assertEqual(AffinePoint(702787153408, 513816894152), point_mul_2)
+        self.assertEqual(AffinePoint(748235734737, 753279782927), point_mul_3)
+
+    def test_add(self):
+        # Arrange.
+        point_1 = AffinePoint(172235452673, 488838007757)
+        point_2 = AffinePoint(748235734737, 753279782927)
+
+        # Act.
+        point_add_1 = point_1 + point_2
+        point_add_2 = point_1 + point_1
+
+        # Assert.
+        self.assertEqual(AffinePoint(198482119007, 191241681320), point_add_1)
+        self.assertEqual(AffinePoint(215387987039, 765000578277), point_add_2)
+
+
+if __name__ == "__main__":
+    unittest.main()
