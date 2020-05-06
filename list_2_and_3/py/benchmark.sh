ab5a2cb0af4720e368fb5320d9b644cef89365dd
Piotr Szyma <thompson2908@gmail.com>
2020-05-06 19:45:27 +0200

    fixes

--- list_2_and_3/py/benchmark.sh
+++ list_2_and_3/py/benchmark.sh
@@ -1,5 +1,12 @@
-echo "projective 20 bits"
-for i in `seq 10`; do python3 -O  main.py --type projective --path params_20.json --value_to_find=3 --time_only; done
+function run_ten_times {
+  echo $1 $2" bits"
+  for i in `seq 10`; do python3 -O  main.py --type $1 --path params_$2.json --value_to_find=3 --time_only; done
 
-echo "affine 20 bits"
-for i in `seq 10`; do python3 -O  main.py --type affine --path params_20.json --value_to_find=3 --time_only; done
+}
+
+run_ten_times projective 20
+run_ten_times affine 20
+run_ten_times projective 40
+run_ten_times affine 40
+run_ten_times projective 60
+run_ten_times affine 60
