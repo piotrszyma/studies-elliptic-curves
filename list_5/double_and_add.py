c128fe3539a9f3482e61782829f3796631b0811c
piotrek <piotr.w.kawa@gmail.com>
2020-06-01 22:00:41 +0200

    Double and add

--- list_5/double_and_add.py
+++ list_5/double_and_add.py
@@ -1,6 +1,6 @@
 import argparse
 import random
-
+from field import FieldInt
 import projective
 import setup
 
@@ -11,15 +11,27 @@ def main(args):
     curve_params, bit_length = setup.set_curve_params(args)
     k = args.k if args.k is not None else random.getrandbits(bit_length)
     base_point = ProjectivePoint(
-      x=curve_params.base_point.x,
-      y=curve_params.base_point.y,
-      z=curve_params.base_point.z,
+        x=curve_params.base_point.x,
+        y=curve_params.base_point.y,
+        z=curve_params.base_point.z,
     )
     mul_point = base_point * k
     print(f"k * G = {mul_point} k = {k} G = {base_point}")
-    
     # TODO: Convert ProjectivePoint to JacobianPoint
     # TODO: Calculate double-and-add method.
+    result = double_and_add(base_point, k)
+    print(result)
+
+
+def double_and_add(point, scalar):
+    scalar = bin(scalar)[2:]
+    TWO = FieldInt(2)
+    result = point
+    for i in range(len(scalar)-1, -1, -1):
+        result = result * TWO
+        if scalar[i] == "1":
+            result += point
+    return result
 
 
 def parse_args():
